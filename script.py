203baee75b384a4035c9d6f832fd7b045ac262ba
idb-antonioalanis <antoniojose.alanisbernal.practicas@telefonica.com>
2024-05-30 13:05:32 +0200

    refactor: Refactor script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import difflib
 import re
 
-FIELDS = [
+REFERENCE_HEADER = [
     'id', 'mac', 'dhcp60', 'hostname', 'dhcp55', 'device_category',
     'device_type', 'device_name', 'device_os', 'device_manufacturer',
     'device_additional_info', 'inference_key', 'random_mac', 'candidates_categories'
@@ -61,7 +61,7 @@ def delete_unnecessary_mapped_fields(mapped_fields):
         If there is a mapped field that is not in the list of known fields or is a duplicate, it will be removed from the list of mapped fields.
     """
     for field in mapped_fields:
-        if field not in FIELDS:
+        if field not in REFERENCE_HEADER:
             mapped_fields.remove(field)
 
     mapped_fields = list(set(mapped_fields))
@@ -69,33 +69,45 @@ def delete_unnecessary_mapped_fields(mapped_fields):
     return mapped_fields
 
 
-def map_fields(fields):
+def map_fields(fields, reference_header):
     """
         Maps a list of input fields to the closest matching fields in the list of known fields.
     """
-    mapped_fields = [find_best_match(field, FIELDS) for field in fields]
+    mapped_fields = [
+        find_best_match(field, reference_header) for field in fields
+    ]
     mapped_fields = delete_unnecessary_mapped_fields(mapped_fields)
 
     return mapped_fields
 
 
+def is_valid_header(header, reference_header):
+    differences = set(header) ^ set(reference_header)
+
+    if differences:
+        print(f"Invalid header. {differences} not in {reference_header}.")
+
+        return False
+
+    print("Valid header.")
+    return True
+
+
 if __name__ == "__main__":
-    second_list = [
+    another_header = [
         'Categorization date', 'MAC', 'DHCP 60', 'DHCP 55', 'Host name',
         'Device category', 'Device type', 'Device name', 'Device OS',
         'Device manufacturer', 'Is it a random mac?', 'Inference key',
-        'Device additional info', 'Rules file name', 'Candidates'
+        'Device additional info', 'Rules file name', 'Candidates', 'Extra field'
     ]
 
-    print(f"First list: {FIELDS}", end="\n\n")
+    print(f"Reference header: {REFERENCE_HEADER}", end="\n\n")
 
-    print(f"Second list: {second_list}", end="\n\n")
+    print(f"Another header: {another_header}", end="\n\n")
 
-    mapped_fields = map_fields(second_list)
+    another_header_mapped = map_fields(another_header, REFERENCE_HEADER)
 
-    print(f"Mapped fields from Second list: {mapped_fields}", end="\n\n")
+    print(f"Mapped fields from Another header: {
+          another_header_mapped}", end="\n\n")
 
-    print(f"Is the mapped fields list the same as the known fields list? {
-        mapped_fields == FIELDS}")
-
-    print(f"Their difference is: {set(mapped_fields) ^ set(FIELDS)}")
+    is_valid_header(another_header_mapped, REFERENCE_HEADER)
